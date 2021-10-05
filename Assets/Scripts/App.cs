1daeba7a299152a60c2a9377c4999374a42ea51e
Dmitriy Prikhodko <bojlahg@gmail.com>
2021-10-06 00:48:45 +0500

    finished

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -5,18 +5,22 @@ using UnityEngine.UI;
 
 public class App : MonoBehaviour
 {
-    public Color m_SelectedColor, m_DefaultColor;
+    public Color m_GoodColor, m_BadColor, m_DefaultColor;
     public GameObject m_SettingsScreen, m_CalcScreen, m_ResultScreen;
-    public Toggle m_AddToggle, m_SubToggle, m_MulToggle, m_DivToggle;
-    public Text m_CountText, m_TitleText, m_QuestionText;
+    public Toggle m_AddToggle, m_SubToggle, m_MulToggle, m_DivToggle, m_SndToggle;
+    public Text m_CountText, m_TitleText, m_QuestionText, m_TimerText;
     public Button m_NextButton;
     public Image[] m_ButtonImages;
     public Text[] m_ButtonTexts, m_ResultTexts;
     public GameObject[] m_Results;
     public string[] m_OperatorStrings;
+    public AudioSource m_AudioSource;
+    public AudioClip[] m_GoodSounds, m_BadSounds;
     //
+    private float m_Timer = 0;
     private int[] m_ResultGood = new int[5], m_ResultCount = new int[5];
-    private int m_IterValue = 10, m_TotalCount = 10, m_RightAnswerIndex = 0, m_AnswerIndex = -1, m_Index = 0, m_OperIndex = 0;
+    private int m_IterValue = 10, m_TotalCount = 10, m_RightAnswerIndex = 0, m_Index = 0, m_OperIndex = 0;
+    private bool m_Answered = false, m_SoundEnabled = true;
     private List<int> m_Operations = new List<int>();
 
     private void Awake()
@@ -28,10 +32,28 @@ public class App : MonoBehaviour
         m_SettingsScreen.SetActive(true);
         m_CalcScreen.SetActive(false);
         m_ResultScreen.SetActive(false);
+
+        m_AddToggle.isOn = PlayerPrefs.GetInt("OpAdd", 1) == 1 ? true : false;
+        m_SubToggle.isOn = PlayerPrefs.GetInt("OpSub", 1) == 1 ? true : false;
+        m_MulToggle.isOn = PlayerPrefs.GetInt("OpMul", 1) == 1 ? true : false;
+        m_DivToggle.isOn = PlayerPrefs.GetInt("OpDiv", 1) == 1 ? true : false;
+        m_SndToggle.isOn = PlayerPrefs.GetInt("SndOn", 1) == 1 ? true : false;
+        m_TotalCount = PlayerPrefs.GetInt("Cnt", 10);
+        m_CountText.text = m_TotalCount.ToString();
+    }
+
+    private void Update()
+    {
+        if (!m_Answered)
+        {
+            m_Timer += Time.deltaTime;
+        }
+        m_TimerText.text = string.Format("{0:0.0}", m_Timer);
     }
 
     public void Settings_Start_Button()
     {
+        m_Operations.Clear();
         if (m_AddToggle.isOn)
         {
             m_Operations.Add(0);
@@ -49,6 +71,8 @@ public class App : MonoBehaviour
             m_Operations.Add(3);
         }
 
+        m_SoundEnabled = m_SndToggle.isOn;
+
         if (m_Operations.Count > 0)
         {
             for (int i = 0; i < 5; ++i)
@@ -57,6 +81,15 @@ public class App : MonoBehaviour
                 m_ResultCount[i] = 0;
             }
 
+            PlayerPrefs.SetInt("OpAdd", m_AddToggle.isOn ? 1 : 0);
+            PlayerPrefs.SetInt("OpSub", m_SubToggle.isOn ? 1 : 0);
+            PlayerPrefs.SetInt("OpMul", m_MulToggle.isOn ? 1 : 0);
+            PlayerPrefs.SetInt("OpDiv", m_DivToggle.isOn ? 1 : 0);
+            PlayerPrefs.SetInt("SndOn", m_SndToggle.isOn ? 1 : 0);
+            PlayerPrefs.SetInt("Cnt", m_TotalCount);
+            PlayerPrefs.Save();
+
+            m_Timer = 0;
             m_Index = 0;
             m_SettingsScreen.SetActive(false);
             m_CalcScreen.SetActive(true);
@@ -85,28 +118,45 @@ public class App : MonoBehaviour
 
     public void Calc_Answer_Button(int idx)
     {
-        m_AnswerIndex = idx;
-        for (int i = 0; i < 9; ++i)
+        if (!m_Answered)
         {
-            m_ButtonImages[i].color = i == m_AnswerIndex ? m_SelectedColor : m_DefaultColor;
+            m_Answered = true;
+            if (m_RightAnswerIndex == idx)
+            {
+                ++m_ResultGood[m_OperIndex];
+                ++m_ResultGood[4];
+
+                if(m_SoundEnabled)
+                {
+                    m_AudioSource.clip = m_GoodSounds[Random.Range(0, m_GoodSounds.Length)];
+                    m_AudioSource.Play();
+                }
+            }
+            else 
+            {
+                if (m_SoundEnabled)
+                {
+                    m_AudioSource.clip = m_BadSounds[Random.Range(0, m_BadSounds.Length)];
+                    m_AudioSource.Play();
+                }
+            }
+            ++m_ResultCount[m_OperIndex];
+            ++m_ResultCount[4];
+
+            for (int i = 0; i < 9; ++i)
+            {
+                m_ButtonImages[i].color = i == m_RightAnswerIndex ? m_GoodColor : m_BadColor;
+            }
+            m_NextButton.interactable = true;
         }
-        m_NextButton.interactable = true;
     }
 
     public void Calc_Next_Button()
     {
-        if (m_RightAnswerIndex == m_AnswerIndex)
-        {
-            ++m_ResultGood[m_OperIndex];
-            ++m_ResultGood[4];
-        }
-        ++m_ResultCount[m_OperIndex];
-        ++m_ResultCount[4];
         ++m_Index;
         if (m_Index == m_TotalCount)
         {
-            ShowResults();
-            
+            ShowResults();            
         }
         else
         {
@@ -139,7 +189,8 @@ public class App : MonoBehaviour
         for(int i = 0; i < 5; ++i)
         {
             m_ResultTexts[i].text = string.Format("{0} / {1}", m_ResultGood[i], m_ResultCount[i]);
-        }        
+        }
+        m_ResultTexts[5].text = string.Format("{0:0.0}", m_Timer);
     }
 
     private void SubGen(int oper, out int l, out int r, out int a)
@@ -149,14 +200,14 @@ public class App : MonoBehaviour
         switch (oper)
         {
             case 0: // Add
-                l = Random.Range(1, 99);
-                r = Random.Range(1, 99);
+                l = Random.Range(1, 100);
+                r = Random.Range(1, 100);
                 a = l + r;
                 break;
 
             case 1: // Sub
-                l = Random.Range(1, 99);
-                r = Random.Range(1, 99);
+                l = Random.Range(1, 100);
+                r = Random.Range(1, 100);
                 if(l < r)
                 {
                     tmp = l;
@@ -167,15 +218,18 @@ public class App : MonoBehaviour
                 break;
 
             case 2: // Mul
-                l = Random.Range(0, 10);
-                r = Random.Range(0, 10);
+                l = Random.Range(1, 11);
+                r = Random.Range(1, 11);
                 a = l * r;
                 break;
 
             case 3: // Div
-                l = Random.Range(2, 10);
-                r = Random.Range(2, 10);
-                a = l / r;
+                l = Random.Range(1, 11);
+                r = Random.Range(1, 11);
+                a = l * r;
+                tmp = l;
+                l = a;
+                a = tmp;
                 break;
         }
     }
@@ -216,7 +270,8 @@ public class App : MonoBehaviour
             }
         }
 
-        Shuffle(answers);
+        answers.Sort();
+        //Shuffle(answers);
 
         for(int i = 0; i < answers.Count; ++i)
         {
@@ -235,35 +290,6 @@ public class App : MonoBehaviour
         m_TitleText.text = string.Format("{0} / {1}", m_Index + 1, m_TotalCount);
 
         m_NextButton.interactable = false;
-    }
-
-    private static void Shuffle(List<int> list)
-    {
-        int cnt = 0;
-        while(cnt < 20)
-        {
-            int l = Random.Range(0, list.Count), r = Random.Range(0, list.Count);
-            if(l != r)
-            {
-                int tmp = list[l];
-                list[l] = list[r];
-                list[r] = tmp;
-                ++cnt;
-            }
-        }
-    }
-
-    private static int Wrap(int val, int min, int max)
-    {
-        int size = max - min;
-        val -= min;
-        if (val < 0)
-        {
-            return min + (val % size) + size;
-        }
-        else
-        {
-            return min + val % size;
-        }
+        m_Answered = false;
     }
 }
